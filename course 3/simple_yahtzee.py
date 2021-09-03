4cfb0fcbec70eeea14ee7d983082f64998abc488
Ryan Tang <ztang484@gmail.com>
2021-09-03 16:51:44 -0400

    done course 3 week 4

--- course 3/simple_yahtzee.py
+++ course 3/simple_yahtzee.py
@@ -1,12 +1,23 @@
 """
 Planner for Yahtzee
 Simplifications:  only allow discard and roll, only score against upper level
-"""
 
+Upper level:
+ones, twos, threes, fours, fives, sixes
+"""
 def gen_all_sequences(outcomes, length):
     """
-    Iterative function that enumerates the set of all sequences of
-    outcomes of given length.
+    Iterative function that enumerates the set of all possible
+    sequences of outcomes of given length.
+
+    parameter
+    ---------
+    outcomes: a set of unique outcomes/states
+    length: int, number of sequence to generate
+
+    return
+    ------
+    set([(..), (..), ....])
     """
     
     answer_set = set([()])
@@ -26,11 +37,19 @@ def score(hand):
     Compute the maximal score for a Yahtzee hand according to the
     upper section of the Yahtzee score card.
 
-    hand: full yahtzee hand
+    parameter
+    ---------
+    hand: sorted tuple, full yahtzee hand
 
-    Returns an integer score 
+    return
+    -------
+    an integer score 
     """
-    return 0
+    max_score = 0
+    for die in set(hand):
+        max_score = max(hand.count(die) * die, max_score)
+
+    return max_score
 
 
 def expected_value(held_dice, num_die_sides, num_free_dice):
@@ -38,25 +57,64 @@ def expected_value(held_dice, num_die_sides, num_free_dice):
     Compute the expected value based on held_dice given that there
     are num_free_dice to be rolled, each with num_die_sides.
 
+    parameter
+    ---------
     held_dice: dice that you will hold
     num_die_sides: number of sides on each die
     num_free_dice: number of dice to be rolled
 
-    Returns a floating point expected value
+    return
+    ------
+    expected value of the entire hand
+        # of held_dice + num_free_dice = # of entire hand of dice
     """
-    return 0.0
+    exv = 0.0
+    prob = (1./num_die_sides) ** num_free_dice  # iid prob for each possibile pair
+
+    for pair in gen_all_sequences(range(1, num_die_sides+1), num_free_dice):
+        hand = held_dice + pair
+        pair_score = score(hand)
+        exv += pair_score * prob
+
+    return exv
 
 
 def gen_all_holds(hand):
     """
-    Generate all possible choices of dice from hand to hold.
+    Generate all possible hold choices of dice given a hand.
+    A hand does not neccessary need to be unique values, ex. [1,1,2,2,3]
 
+    parameter
+    ---------
     hand: full yahtzee hand
 
-    Returns a set of tuples, where each tuple is dice to hold
+    return
+    ------
+    a set of tuples, where each tuple is a set of dice to hold
     """
-    return set([()])
+    all_holds = set([()])  # start with empty subset
+    hand_die_cnt = {die:hand.count(die) for die in set(hand)}
 
+    for length in range(1, len(hand) + 1):
+        all_seq = gen_all_sequences(hand, length)
+        cor_seq = []
+        for seq in all_seq:
+            # remove all seq with impossible dice count
+            # ex. hand(2,1), seq(2,2)
+            skip = False
+            for die in set(seq):
+                if seq.count(die) > hand_die_cnt[die]:
+                    skip = True
+                    break
+            
+            if not skip:
+                # order does not matter, remove duplicates
+                sorted_seq = tuple(sorted(seq))
+                cor_seq.append(sorted_seq)
+
+        all_holds.update(cor_seq)
+
+    return all_holds
 
 
 def strategy(hand, num_die_sides):
@@ -64,13 +122,26 @@ def strategy(hand, num_die_sides):
     Compute the hold that maximizes the expected value when the
     discarded dice are rolled.
 
+    parameter
+    ---------
     hand: full yahtzee hand
     num_die_sides: number of sides on each die
 
-    Returns a tuple where the first element is the expected score and
-    the second element is a tuple of the dice to hold
+    return
+    ------
+    (expected score, a tuple of the dice to hold)
     """
-    return (0.0, ())
+    sides = num_die_sides
+
+    best_stgy, best_val = None, None
+    for hold in gen_all_holds(hand):
+        nfree = len(hand) - len(hold)
+        val = expected_value(hold, sides, nfree)
+        
+        if best_stgy is None or best_val is None or val > best_val:
+            best_stgy, best_val = hold, val
+
+    return best_val, best_stgy
 
 
 def run_example():
@@ -82,16 +153,6 @@ def run_example():
     hand_score, hold = strategy(hand, num_die_sides)
     print "Best strategy for hand", hand, "is to hold", hold, "with expected score", hand_score
     
-    
-run_example()
 
 
-#import poc_holds_testsuite
-#poc_holds_testsuite.run_suite(gen_all_holds)
-                                       
-    
-    
-    
-
-
-
+# run_example()
--- course 3/test_simple_yahtzee.py
+++ course 3/test_simple_yahtzee.py
@@ -1,2 +1,74 @@
-def testa():
-    assert 100
+import pytest
+from pytest import approx
+from simple_yahtzee import score, expected_value, gen_all_holds, strategy
+
+
+@pytest.mark.parametrize(
+    'hand, expected',
+    [
+        ((1,2,3), 3),
+        ((1,1,2), 2),
+        ((1,1,1), 3),
+        ((6,6,6), 18),
+        ((4,4,6), 8),
+    ],
+)
+def test_score(hand, expected):
+    assert score(hand) == expected
+
+
+@pytest.mark.parametrize(
+    'held, sides, nfree, expected',
+    [
+        ((1,2,), 6, 1, approx(4.0, rel=0.01)),
+        ((6,6,), 6, 1, approx(13.0, rel=0.01)),
+        ((5,5,5), 6, 2, approx(50./3, rel=0.01)),
+        ((), 6, 1, approx(3.5, rel=0.01)),
+        ((), 4, 1, approx(2.5, rel=0.01)),
+        ((), 4, 2, approx(3.75, rel=0.01)),
+        ((3,3), 8, 5, approx(11.3590087891, rel=0.01)),
+    ],
+)
+def test_expected_value(held, sides, nfree, expected):
+    assert expected_value(held, sides, nfree) == expected
+
+
+@pytest.mark.parametrize(
+    'hand, expected',
+    [
+        ((1,), 2),
+        ((1,1), 3),
+        ((2,3), 4),
+        ((), 1),
+        ((7,8,8,4), 12),
+        ((5,5), 3),
+    ],
+)
+def test_gen_all_holds_by_num(hand, expected):
+    assert len(gen_all_holds(hand)) == expected
+
+
+@pytest.mark.parametrize(
+    'hand, expected',
+    [
+        ((1,), set([(), (1,)])),
+        ((1,1), set([(), (1,), (1,1)])),
+        ((2,3), set([(), (2,), (3,), (2,3)])),
+        ((5,5), set([(5,), (), (5,5)])),
+    ]
+)
+def test_gen_all_holds_by_exact_set(hand, expected):
+    assert gen_all_holds(hand) == expected
+
+
+@pytest.mark.parametrize(
+    'hand, sides, expected',
+    [
+        ( (6,6,4), 6, [(approx(13.0, rel=0.01), (6,6))] ),
+        ( (7,2), 8, [(approx(8.0, rel=0.01), (7,))] ),
+        ( (1,), 6, [(approx(3.5, rel=0.01), ())]),
+    ]
+)
+def test_strategy(hand, sides, expected):
+    assert strategy(hand, sides) in expected
+
